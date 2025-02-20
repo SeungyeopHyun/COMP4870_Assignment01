c8e13120d7c495d3f3ac90468a8069bcdd0e4bbb
Seungyeop Hyun <shyun9@my.bcit.ca>
2025-02-19 17:50:44 -0800

    modify program.cs

--- Program.cs
+++ Program.cs
@@ -3,38 +3,24 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// SQLite 연결
+// Azure 환경변수에서 ConnectionString 불러오기
+var connectionString = Environment.GetEnvironmentVariable("DefaultConnection") 
+                       ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(connectionString));
 
-// MVC 서비스 등록
-builder.Services.AddControllersWithViews();
-AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(30);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
-
-builder.Services.AddAntiforgery();  // CSRF 보호 활성화
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-app.UseSession();
 app.UseAuthorization();
-app.UseAntiforgery();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
