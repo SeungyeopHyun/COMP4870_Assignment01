bb5e52c67e3d153a5f6333d141ee9413476dc6ad
Seungyeop Hyun <shyun9@my.bcit.ca>
2025-02-19 18:46:12 -0800

    appsetting.json fixed

--- obj/Debug/net9.0/BlogWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlogWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1ddf8e12c88256e0df2cb7db23a6287746eb51e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784dbcc69302169c91c5ed2354cb8cdfac70f3d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
