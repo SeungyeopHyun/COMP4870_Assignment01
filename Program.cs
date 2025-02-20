f3a69a61819c45466277be0730630c2a53c230f8
Seungyeop Hyun <shyun9@my.bcit.ca>
2025-02-19 17:56:40 -0800

    fix program.cs

--- Program.cs
+++ Program.cs
@@ -3,21 +3,34 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Azure 환경변수에서 ConnectionString 불러오기
-var connectionString = Environment.GetEnvironmentVariable("DefaultConnection") 
-                       ?? builder.Configuration.GetConnectionString("DefaultConnection");
-
+// SQLite 연결
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// MVC 서비스 등록
+builder.Services.AddControllersWithViews();
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddAntiforgery();  // CSRF 보호 활성화
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
+app.UseSession();
 app.UseAuthorization();
+app.UseAntiforgery();
 
 app.MapControllerRoute(
     name: "default",
